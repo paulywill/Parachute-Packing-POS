7bf2a373fabb19da512fba0af5b74977cd6b5352
Paul Gamble <pgamble@gmail.com>
2012-12-06 16:49:16 -0500

    Add links to Rigs, still have to resolve Rigs Show view to find all packjobs for that rig

--- app/models/packjob.rb
+++ app/models/packjob.rb
@@ -1,19 +1,22 @@
 class Packjob < ActiveRecord::Base
-   attr_accessible :pj_damage, :pj_extranotes, :pj_guestpacker, :pj_packer, :pj_rig_type_number, :packer_id
+   attr_accessible :pj_damage, :pj_extranotes, :pj_guestpacker, :pj_packer, :pj_rig_type_number, :packer_id, :rig_id
 
    validates :packer_id, :presence  => true
-   validates :pj_rig_type_number, :presence  => true
+   validates :rig_id, :presence  => true
    
    belongs_to :packer
+   belongs_to :rig
 
    before_save :set_pj_packer
-
+   before_save :set_pj_rig_type_number	
+   
    def set_pj_packer
       self.pj_packer = packer.try(:p_name)
    end	
 
+
+   def set_pj_rig_type_number
+      self.pj_rig_type_number = rig.try(:rig_type_number)
+   end
    
-
-
-
 end
